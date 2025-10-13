ab75020bbecef063f1eff430704ef1aa777bcea9
Subham Sharma <ssuvam670@gmail.com>
2025-10-13 06:15:26 +0100

    update

---
+++ tests/GenerateTranslationsTest.php
@@ -0,0 +1,7 @@
+<?php
+it('can generate translation files', function () {
+    $this->artisan('auto-translations:generate --force')
+         ->assertSuccessful();
+    
+    expect(file_exists(resource_path('lang/en.json')))->toBeTrue();
+});
